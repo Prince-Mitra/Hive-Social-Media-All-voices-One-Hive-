c5e13d5e2c8858503dafc6212dd1ae31bff36e4f
Prince-Mitra <pm9190879@gmail.com>
2025-09-24 10:45:27 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://hive-ie0g.onrender.com",
         methods:["GET","POST"]
     }
 })
